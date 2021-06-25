d9c15f709138cfe42b99a635272d6598c7c283fc
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-25 14:37:49 +0530

    V1

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,20 +1,21 @@
 /**Function add(): adds two numbers from the input and displays the answer */
 function add() {
+  //take number 1 from input box "n1" and store it in a variable named "input1"
+  var input1 = Number(document.getElementById("n1").value);
+  //take number 1 from input box "n1" and store it in a variable named "input1"
+  var input2 = Number(document.getElementById("n2").value);
+  //add input 1 and input 2 and store it in a variable named "result"
+  var result = input1 + input2;
 
-    //take number 1 from input box "n1" and store it in a variable named "input1"
-    var input1=Number(document.getElementById("n1").value);
-     //take number 1 from input box "n1" and store it in a variable named "input1"
-    var input2=Number(document.getElementById("n2").value)
-    //add input 1 and input 2 and store it in a variable named "result"
-    var result = input1 + input2;
-
-    console.log(result);
-    // display the result in an HTML element with id "answer" 
-    document.getElementById("answer").innerHTML=result;
+  console.log(result);
+  // display the result in an HTML element with id "answer"
+  document.getElementById("answer").innerHTML = result;
 }
-function subtract(){
-    
-}
+/**Function subtract(): subtracts two numbers from the input and displays the answer */
+function subtract() {}
 
-    
-    
+/**Function divide(): divides two numbers from the input and displays the answer */
+function divide() {}
+
+/**Function multiply(): multiplies two numbers from the input and displays the answer */
+function multiply() {}
