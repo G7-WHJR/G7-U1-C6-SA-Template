95de8ad9703be48ea41899284ede42cc70d472a7
unknown <raveen.anand@whitehatjr.com>
2021-10-22 14:37:25 +0530

    v2

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,21 +1,54 @@
-/**Function add(): adds two numbers from the input and displays the answer */
+/**add(): adds two numbers from the input and displays the answer */
 function add() {
-  //take number 1 from input box "n1" and store it in a variable named "input1"
+  //1. Take number 1 from input box "n1" and store it in a variable named "input1"
   var input1 = Number(document.getElementById("n1").value);
-  //take number 1 from input box "n1" and store it in a variable named "input1"
+  
+  //2. Take number 2 from input box "n2" and store it in a variable named "input2"
   var input2 = Number(document.getElementById("n2").value);
-  //add input 1 and input 2 and store it in a variable named "result"
+  
+  //3. Add input 1 and input 2 and store it in a variable named "result"
   var result = input1 + input2;
-
-  console.log(result);
-  // display the result in an HTML element with id "answer"
+  
+  //4.Display the result in an HTML element with id "answer"
   document.getElementById("answer").innerHTML = result;
 }
-/**Function subtract(): subtracts two numbers from the input and displays the answer */
-function subtract() {}
 
-/**Function divide(): divides two numbers from the input and displays the answer */
-function divide() {}
+/**subtract(): subtracts two numbers from the input and displays the answer */
+function subtract() {
 
-/**Function multiply(): multiplies two numbers from the input and displays the answer */
-function multiply() {}
+ //1. Take number 1 from input box "n1" and store it in a variable named "input1"
+ var input1 = Number(document.getElementById("").value);
+ 
+ //2. Take number 2 from input box "n2" and store it in a variable named "input2"
+ var input2 = Number(document.getElementById("").value);
+ 
+ //3. Subtract input 2 from input 1 and store it in a variable named "result"
+ var result = ;
+ 
+ //4.Display the result in an HTML element with id "answer"
+ document.getElementById("").innerHTML = ;
+  
+}
+
+/**divide(): divides two numbers from the input and displays the answer */
+function divide() {
+ //1. Take number 1 from input box "n1" and store it in a variable named "input1"
+ 
+ //2. Take number 2 from input box "n2" and store it in a variable named "input2"
+ 
+ //3. Divide "input2" by "input1" and store it in a variable named "result"
+ 
+ //4. Display the result in an HTML element with id "answer"
+}
+
+/**multiply(): multiplies two numbers from the input and displays the answer */
+function multiply() {
+
+//1. Take number 1 from input box "n1" and store it in a variable named "input1"
+ 
+ //2. Take number 2 from input box "n2" and store it in a variable named "input2"
+ 
+ //3. Multiply "input1" and "input2" and store it in a variable named "result"
+ 
+ //4. Display the result in an HTML element with id "answer"
+}
