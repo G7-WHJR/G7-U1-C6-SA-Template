735301e12274b7229a70ec1bc585d813cc712d9c
unknown <raveen.anand@whitehatjr.com>
2021-10-22 14:48:19 +0530

    v2

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,7 +6,7 @@ function add() {
   //2. Take number 2 from input box "n2" and store it in a variable named "input2"
   var input2 = Number(document.getElementById("n2").value);
   
-  //3. Add input 1 and input 2 and store it in a variable named "result"
+  //3. Add "input1" and "input2" and store it in a variable named "result"
   var result = input1 + input2;
   
   //4.Display the result in an HTML element with id "answer"
@@ -22,7 +22,7 @@ function subtract() {
  //2. Take number 2 from input box "n2" and store it in a variable named "input2"
  var input2 = Number(document.getElementById("").value);
  
- //3. Subtract input 2 from input 1 and store it in a variable named "result"
+ //3. Subtract "input2" from "input1" and store it in a variable named "result"
  var result = ;
  
  //4.Display the result in an HTML element with id "answer"
